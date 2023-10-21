178db18cba7a27435b43c58a73364b29e3e44dba
vbavalatti <vijay.nb@gmail.com>
2023-10-21 20:11:12 +0530

    https://github.com/vbavalatti/web-01.git

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add your JavaScript code here
+
+document.getElementById("contactButton").addEventListener("click", function() {
+    alert("Please email us at contact@xyzschool.com for any inquiries.");
+});
